640ba1f5bb30483e1ca35a625f8a2fda8f09bbc6
labankibet <laban.kibet@moringaschool.com>
2023-09-19 08:39:13 +0300

    putting it all together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    #initialize attributes
+    def __init__(self, title, page_count=None):
+        self.title = title
+        self._page_count = 0
+        if page_count is not None:
+            self.set_page_count(page_count)
+    
+    #get and set the page        
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+            
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    page_count = property(get_page_count, set_page_count)
+
+book = Book("And Then There Were None")
+book.page_count = 90
+print(book.page_count)
+book.turn_page()
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    #initialize attributes
+    def __init__(self,brand,size=None):
+        self.condition ="New"
+        self.brand = brand
+        self._size = 0
+        if self._size is not None:
+            self.set_size(size)
+    
+    #get and set the size of the shoe accordingly       
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) == int:
+            self._size = size
+        else:
+            print("size must be an integer")
+    #defining cobble method        
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        
+    size = property(get_size, set_size)
     pass
